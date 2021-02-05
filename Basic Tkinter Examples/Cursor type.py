053e4910186a6b3f11c82fd58b4c7e777a273d52
Bhupesh Sharma <76774031+bsthedev@users.noreply.github.com>
2021-02-05 21:42:21 +0530

    Add files via upload

---
+++ Basic Tkinter Examples/Cursor type.py
@@ -0,0 +1,24 @@
+from tkinter import *
+  
+#Create Tkinter window 
+frame = Tk() 
+frame.title('Cursor Types') 
+frame.geometry('200x200') 
+
+#Cursor type plus on widget
+frame.config(cursor = "plus")
+
+#Xterm type cursor on Text  
+L1 = Label(frame, text= "Text", cursor = "xterm").pack() 
+
+#Circle type cursor on Circle button 
+B1 = Button(frame, text = "Circle Cursor", cursor = "circle").pack() 
+
+#Heart type cursor on Plus button  
+B2 = Button(frame, text = "Heart cursor", cursor = "heart").pack() 
+
+#Dot type cursor on Exit button  
+a = Button(frame, text = 'Exit', cursor = "dot", 
+              command = frame.destroy).pack() 
+  
+frame.mainloop() 
